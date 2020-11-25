0ce7cbc79ee664c7bfbc19b1ced463de84b4e1e1
Diana Utan <didiutzi@gmail.com>
2020-11-25 22:46:16 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,14 +1,14 @@
 # Use this space to code along with the readme
 def put_phrase_ten_times 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 puts phrase
 end
